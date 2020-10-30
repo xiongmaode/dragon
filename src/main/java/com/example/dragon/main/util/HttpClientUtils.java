10e0e1408a8c2ac5c0a62a91ba0824de23a7e226
songxinwei@dinsmooth.com <songxinwei>
2020-10-30 14:30:19 +0800

    http

--- src/main/java/com/example/dragon/main/util/HttpClientUtils.java
+++ src/main/java/com/example/dragon/main/util/HttpClientUtils.java
@@ -7,12 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.ConnectException;
-
 /**
  * 本类负责调用接口
- *
- * @version：1.0
  */
 @Slf4j
 public class HttpClientUtils {
@@ -25,45 +21,51 @@ public class HttpClientUtils {
     }
 
     /**
-     * @Description post请求
-     * @Param [uuid, url, data]
-     * @Return java.lang.String
+     * post请求
      */
-    public static String postTemplate(String uuid,String url, Object data) throws ConnectException {
-        return postTemplate(uuid,url,data,"utf-8",60000);
+    public static String postTemplate(String url, Object data) {
+        return restTemplate(url, data, "post");
+    }
+
+    /**
+     * get请求
+     */
+    public static String getTemplate(String url) {
+        return restTemplate(url, null, "get");
     }
 
     /**
      * restTemplate请求
-     *
-     * @return
-     * @throws ConnectException
      */
-    public static String postTemplate(String uuid, String url, Object data, String encodeType,int timeOut) throws ConnectException {
-        log.info("【uuid:" + uuid + "】调用第三方接口地址:" + url);
-        log.info("【uuid:" + uuid + "】请求报文:" + JsonUtils.bean2json(data));
-        log.info("【uuid:" + uuid + "】请求开始，超时时间:" + timeOut);
+    private static String restTemplate(String url, Object data, String type) {
+        log.info("【调用第三方接口地址:" + url);
+        log.info("【请求开始，超时时间:" + 60000);
         String resultStr = "";
         try {
             //HttpRequestFactory初始化请求参数
             SimpleClientHttpRequestFactory httpRequestFactory = new SimpleClientHttpRequestFactory();
-            httpRequestFactory.setReadTimeout(timeOut);
-            httpRequestFactory.setConnectTimeout(timeOut);
+            httpRequestFactory.setReadTimeout(60000);
+            httpRequestFactory.setConnectTimeout(60000);
             //创建请求对象
             RestTemplate restTemplate = new RestTemplate(httpRequestFactory);
-            //设置请求报文头参数
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.set("Accept-Charset", encodeType);
-            HttpEntity<Object> request = new HttpEntity<Object>(data, headers);
-            Object resStr = restTemplate.postForObject(url, request, Object.class);
+            Object resStr;
+            if ("post".equals(type)) {
+                log.info("【请求报文:" + JsonUtils.bean2json(data));
+                //设置请求报文头参数
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                headers.set("Accept-Charset", "utf-8");
+                HttpEntity<Object> request = new HttpEntity<>(data, headers);
+                resStr = restTemplate.postForObject(url, request, Object.class);
+            } else {
+                resStr = restTemplate.getForObject(url, Object.class);
+            }
             resultStr = JsonUtils.bean2json(resStr);
-        }catch (Exception e){
-            log.error(uuid, e);
-            throw new ConnectException("第三方接口连接失败");
+        } catch (Exception e) {
+            log.error("【第三方接口连接失败", e);
         } finally {
-            log.info("【uuid:" + uuid + "】发送请求结束，返回报文：" + resultStr);
-            log.info("【uuid:" + uuid + "】调用第三方接口结束");
+            log.info("【发送请求结束，返回报文：" + resultStr);
+            log.info("【调用第三方接口结束");
         }
         return resultStr;
     }
